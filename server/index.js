c392eef26a73e83a82ae50d4403fa5079670a8bd
dohendra <bholubabu420@gmail.com>
2024-03-30 01:43:46 +0530

    server : connected to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import formRoutes from './routes/formRoutes.js';
 import errorHandler from './middleware/errorHandler.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri =  process.env.MONGO;
+mongoose.connect(uri).then(()=>{console.log("connected to mongo");});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
