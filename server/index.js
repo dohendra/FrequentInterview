43c9a356a88ab6237fb153bc6eb33bf003a6f192
dohendra <bholubabu420@gmail.com>
2024-04-01 14:03:46 +0530

    add pre preproduction changes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import errorHandler from './middleware/errorHandler.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
 
 const uri = process.env.MONGO;
@@ -12,6 +13,18 @@ mongoose.connect(uri).then(()=>{console.log("connected to mongo");}).catch(err =
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.status(200).send('Web service is live!');
+});
+// Serve static files from the React app build directory
+const __dirname=path.resolve();
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'client','build','index.html'));
+});
 app.use(cors());
 app.use(express.json());
 app.use('/api', formRoutes);
